07ca031ff750974917e694eccb7dc97e1418a8a6
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-01-17 00:04:53 -0300

    delete contact

--- src/backend/routes/contact.routes.js
+++ src/backend/routes/contact.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { listContactsByUserId, createNewContact, updateContact }  = require('../controllers/contactController');
+const { listContactsByUserId, createNewContact, updateContact, deleteContact }  = require('../controllers/contactController');
 const { validatePhone, validateName, validateEmailIsInUseByOtherContact } = require('../middlewares/validation.contacts');
 const validateToken = require('../middlewares/validate.jwt');
 
@@ -8,5 +8,6 @@ const contactRouter = express.Router();
 contactRouter.get('/list-contacts', validateToken, listContactsByUserId);
 contactRouter.post('/register', validateToken, validateName, validatePhone, validateEmailIsInUseByOtherContact, createNewContact);
 contactRouter.put('/update', validateToken, validateName, validatePhone, validateEmailIsInUseByOtherContact, updateContact);
+contactRouter.delete('/:id', validateToken, deleteContact);
 
 module.exports = contactRouter;
