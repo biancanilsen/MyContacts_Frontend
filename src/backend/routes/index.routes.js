b9f52c2e31d5ac909e34b643e17a06e3cf79c9e7
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-02-09 10:57:45 -0300

    removing backend

--- src/backend/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const contactsRouter = require('./contact.routes');
-const userRouter = require('./user.routes');
-
-const routerIndex = Router();
-routerIndex.use('/contacts', contactsRouter);
-routerIndex.use('/user', userRouter);
-
-module.exports = routerIndex;
