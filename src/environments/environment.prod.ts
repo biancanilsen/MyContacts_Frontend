d01956f85d619e0a8e940bb785587b14fd380910
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-02-13 16:04:32 -0300

    Alterações para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://app-my-contact-backend.herokuapp.com/',
-    loginRoute: 'https://app-my-contact-frontend.herokuapp.com/login'
+    loginRoute: 'https://my-contacts-frontend.vercel.app/login'
 };
